18b26e782577778172a88c0dabef967df3506634
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2023-10-08 17:29:16 -0400

    API side programming
    The HTTP get and post functions that will handle the HTTP requests.

--- jerry-first-online-notepad/Controllers/DiaryController.cs
+++ jerry-first-online-notepad/Controllers/DiaryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
+using Google;
 
 namespace jerry_first_online_notepad.Controllers
 {
@@ -11,12 +12,54 @@ namespace jerry_first_online_notepad.Controllers
     {
         //stores the name of the Storage Bucket
         private const string bucket = "jerrys-hello-world-notepad";
+        //stores the name of the file in the storage bucket
+        private string fileName = "diary.txt";
         //stores the client to access the Storage Bucket
         private StorageClient client;
 
         //constructor to initalize the client
         public DiaryController(StorageClient storageClient) {
             this.client = storageClient;
+        }
+
+        //this function will download the diary to the Http request in the API
+        [HttpGet]
+        public IActionResult GetDiary() {
+            //potential exception
+            try {
+                //create a memory stream
+                using var ms = new MemoryStream();
+                //download diary.txt to said memory stream through an authorized client
+                client.DownloadObject(bucket, fileName, ms);
+                //reset the stream Position
+                ms.Position = 0;
+                //return said stream using File(Stream stream, string contentType)
+                return File(ms, "text/plain");
+            }
+            //catch the not Found Exception
+            catch (Google.GoogleApiException ex) when (ex.Error.Code == 404)
+            {
+                return NotFound("The diary file does not exist on the server.");
+            }
+            //catch any other errors
+            catch (Exception ex){
+                return StatusCode(500, "Internal server error:"+ex.Message);
+            }
+        }
+
+        //this function will upload content from the api to the bucket
+        [HttpPost]
+        public async IActionResult UpdateDiary() {
+            //create a memory stream 
+            using var ms = new MemoryStream();
+            //copy the content from the HTTP request to the stream
+            await Request.Body.CopyToAsync(ms);
+            //reset the position of the stream 
+            ms.Position = 0;
+            //upload from the stream to the storage bucket
+            client.UploadObject(bucket,fileName,null,ms);
+            //return type
+            return Ok("Diary Updated Successfully");
         }
 
     }
